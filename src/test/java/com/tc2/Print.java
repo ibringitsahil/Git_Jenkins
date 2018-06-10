66b3317cdb936219e7375cbe70c7d7eeac6522e4
Sahil3 <ibringitsahil@gmail.com>
2018-06-11 00:08:53 +0530

    Commit by Developer 3

--- src/test/java/com/tc2/Print.java
+++ src/test/java/com/tc2/Print.java
@@ -10,8 +10,19 @@ public class Print {
 		}
 	}
 	
+	public void print2(){
+		int[] arr = {2,4,6};
+		
+		for(int a: arr){
+			System.out.println(a);
+		}
+	}
+	
 	public static void main(String[] args) {
 		Print print = new Print();
 		print.print();
+		
+		Print print2 = new Print();
+		print2.print2();
 	}
 }
