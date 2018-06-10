b63466247e5a26f6756534412a65c9447777a1bd
Sahil <Sahil@192.168.2.12>
2018-06-10 16:20:07 +0530

    First Commit
    6/10

---
+++ src/test/java/com/tc2/TestCase1.java
@@ -0,0 +1,28 @@
+package com.tc2;
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+
+	public static WebDriver driver;
+    @BeforeClass
+	public void LoadBrowser() throws IOException {
+		String url = "http://automationpractice.com/index.php";
+
+		System.setProperty("webdriver.chrome.driver",
+				System.getProperty("user.dir") + "\\drivers\\chromedriver (2).exe");
+		driver = new ChromeDriver();
+
+		driver.get(url);
+
+	}
+    @Test
+    public void firstTest(){
+    	
+    }
+}
