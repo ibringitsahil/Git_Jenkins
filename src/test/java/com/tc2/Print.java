efe125e483938ea25743ead4637d136f9f489141
Sahil1 <ibringitsahil@gmail.com>
2018-06-10 23:57:40 +0530

    Commit By First Dev

---
+++ src/test/java/com/tc2/Print.java
@@ -0,0 +1,17 @@
+package com.tc2;
+
+public class Print {
+	
+	public void print(){
+		int[] arr = {2,4,6};
+		
+		for(int i=0;i<arr.length;i++){
+		  System.out.println(arr[i]);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Print print = new Print();
+		print.print();
+	}
+}
