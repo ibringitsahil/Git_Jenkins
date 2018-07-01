43cd1cc43fb70cbe467f2352ac78c158ab7e7d2a
ibringitsahil <32077170+ibringitsahil@users.noreply.github.com>
2018-07-01 18:28:53 +0530

    Update Print.java

--- src/test/java/com/tc2/Print.java
+++ src/test/java/com/tc2/Print.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Print {
 	
-	public void print(){/*
+	public void print(){
 		int[] arr = {1,5,3,6};
 		
 		for(int i =0 ; i<arr.length;i++){
@@ -12,7 +12,7 @@ public class Print {
 				
 			}
 		}
-	*/}
+	}
 	
 
 	
