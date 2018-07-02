b9404db55d9d7a1ced50e871c431ba9a691f5076
Sahil <ibringitsahil@gmail.com>
2018-07-02 09:03:25 +0530

    7/2

--- src/test/java/com/tc2/Print.java
+++ src/test/java/com/tc2/Print.java
@@ -7,11 +7,11 @@ public class Print {
 	public void print(){
 		int[] arr = {1,5,3,6};
 		
-		for(int i =0 ; i<arr.length;i++){
+	/*	for(int i =0 ; i<arr.length;i++){
 			if(arr[i]<arr[i+1] && arr[i] < arr[i+2] && arr[i < ]){
 				
 			}
-		}
+		}*/
 	}
 	
 
